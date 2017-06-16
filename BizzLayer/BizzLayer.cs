910661e958181ba3de74b187ec9a068dcaf6e945
makowskimarek <makow.mark@gmail.com>
2017-06-16 14:19:37 +0200

    projekt poczatkowy

---
+++ BizzLayer/BizzLayer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// 
+namespace BizzLayer
+{
+    /// <summary>
+    /// ta klasa służy do:
+    /// - jako  klasa dziedzinowa (domenowa) (u Was bedzie pozyskana z mapowania OR)
+    /// - jako klasa do  defniowania wartosci w filtrowaniu
+    /// </summary>
+    public class Patient
+    { 
+        public String FName {get ; set;}
+        public String LName {get ; set;}
+        public DateTime BDate { get; set; }
+        public Decimal Height { get; set; }
+
+    }
+
+    static public class RegistrationFacade
+    {
+        
+        public static List<Patient> GetPatients(Patient searchCrit)
+
+        {
+            var pat = new List<Patient> ();
+            pat.Add(new Patient {FName = "aaaa", LName = "AAAAA", BDate = System.DateTime.Now, Height = 180});
+            pat.Add(new Patient { FName = "aabbaa", LName = "AABBAA", BDate = System.DateTime.Now, Height = 200});
+            pat.Add(new Patient { FName = "bbbb", LName = "BBBBB", BDate = DateTime.Parse("1991-01-01"), Height = 170 });
+            var res = (from element in pat where element.LName.StartsWith(searchCrit.LName) select element).ToList();
+            return res;
+        }
+
+
+    }
+
+
+
+
+}
---
+++ Przychodnia-master/Rejestratorka.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using BizzLayer;
+
+namespace Przychodnia
+{
+    public partial class frmRegistration : Form
+
+    {
+
+        Patient patientSearchCriteria;
+
+        public frmRegistration()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+ 
+            // var result = RegistrationFacade.GetPatients(null);
+ 
+            // budowa kryteriów
+            patientSearchCriteria = new Patient();
+            patientSearchCriteria.LName = textBox1.Text;
+
+            // ładowanie obiektu dataGridView
+            dataGridView1.Columns.Clear();
+            // dataGridView1.AutoGenerateColumns = true;
+            dataGridView1.DataSource = RegistrationFacade.GetPatients(patientSearchCriteria);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Rejestratorka_Load(object sender, EventArgs e)
+        {
+
+           
+        }
+    }
+}
